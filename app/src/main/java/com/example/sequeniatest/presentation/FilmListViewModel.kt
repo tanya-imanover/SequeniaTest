168e546bf8798e6fc90d7fb19287b95a57fb41ac
TI <tanya.imanover@gmail.com>
2024-09-16 17:27:45 +0700

    Add Toolbar and error toast

--- app/src/main/java/com/example/sequeniatest/presentation/FilmListViewModel.kt
+++ app/src/main/java/com/example/sequeniatest/presentation/FilmListViewModel.kt
@@ -11,6 +11,8 @@ import com.example.sequeniatest.domain.GenreSelectedUseCase
 import com.example.sequeniatest.domain.GetFilmListUseCase
 import com.example.sequeniatest.domain.GetGenresUseCase
 import com.example.sequeniatest.domain.LoadDataUseCase
+import com.example.sequeniatest.domain.LoadError
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
 class FilmListViewModel : ViewModel() {
@@ -18,6 +20,10 @@ class FilmListViewModel : ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean>
         get() = _isLoading
+
+    private val _error = MutableLiveData<LoadError>()
+    val error: LiveData<LoadError>
+        get() = _error
 
     private val repository = FilmRepositoryImpl
     private val getFilmListUseCase = GetFilmListUseCase(repository)
@@ -29,13 +35,9 @@ class FilmListViewModel : ViewModel() {
     val filmList = getFilmListUseCase()
     val genres = getGenresUseCase()
 
+
     init {
-        viewModelScope.launch {
-            _isLoading.value = true
-            loadDataUseCase()
-        }.invokeOnCompletion {
-            _isLoading.value = false
-        }
+        loadData()
     }
 
     fun genreSelected(genre: Genre) {
@@ -43,6 +45,21 @@ class FilmListViewModel : ViewModel() {
             genreSelectedUseCase(genre)
         } else {
             genreDeselectedUseCase()
+        }
+    }
+
+    fun loadData(){
+        val handler = CoroutineExceptionHandler { _, throwable -> _error.value = LoadError(true, "Error ${throwable.message}") }
+        viewModelScope.launch(handler) {
+            _isLoading.value = true
+            _error.value = LoadError(false, "")
+            try {
+                loadDataUseCase()
+            } catch (e: Exception){
+                _error.value = LoadError(true, e.message?:"Unknown error")
+            }
+        }.invokeOnCompletion {
+            _isLoading.value = false
         }
     }
 
